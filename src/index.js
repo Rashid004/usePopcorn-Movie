74a9befe1890d4e1475eb7e6ef5a872c05bd3272
Your Name <you@example.com>
2023-12-17 23:54:05 +0530

    usePopcorn Movie WebApp

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 import "./index.css";
 import App from "./App.js";
-// import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -12,12 +11,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// {
-//   /* <StarRating
-//   maxRating={5}
-//   messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-// />
-// <StarRating size={24} color="red" className="test" defaultRating={3} />
-// <Test /> */
-// }
